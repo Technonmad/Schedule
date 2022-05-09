9618e343f695e0b275ddee2793c971e0be8c4300
Anton <65555937+Technonmad@users.noreply.github.com>
2022-05-09 19:26:14 +0300

    DB connection

--- app/src/main/java/com/example/shlepa_schedule/MainActivity.java
+++ app/src/main/java/com/example/shlepa_schedule/MainActivity.java
@@ -1,21 +1,16 @@
 package com.example.shlepa_schedule;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.support.v4.app.*;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
-        implements KursListFragment.Listener{
+
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +18,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public void itemClicked(long id) {
-        Intent intent = new Intent(this, Direction.class);
-        //intent.putExtra(PickGroup.EXTRA_WORKOUT_ID, (int)id);
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, ScheduleActivity.class);
         startActivity(intent);
     }
 }
